22f9171a748510a1bbaf0f2d5ff4a6565b511758
Robin Erickson <re@incuboweb.ca>
2021-02-06 11:40:44 -0500

    reverting failing test

--- __tests__/parse_date_test.go
+++ __tests__/parse_date_test.go
@@ -8,7 +8,7 @@ import (
 func TestParseDate(t *testing.T) {
 	date := utils.ParseDate("2006-01-02T15:04:05.000Z")
 
-	if date != "Monday January 12 2006" {
+	if date != "Monday January 2 2006" {
 		t.Errorf("Date was incorrect, got: %s, want: %s", date, "Monday January 2 2006")
 	}
 }
